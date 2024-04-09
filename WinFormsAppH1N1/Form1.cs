c3bb023ecf1c6d580ec56d105719061149d8e867
Diogo Nunes Vicente <diogonunesv742@gmail.com>
2024-04-09 20:04:41 -0300

    criacao do metodo mover cursor

--- WinFormsAppH1N1/Form1.cs
+++ WinFormsAppH1N1/Form1.cs
@@ -1,7 +1,25 @@
+using System.Runtime.InteropServices;
+
 namespace WinFormsAppH1N1
 {
     public partial class Form1 : Form
     {
+        private const int MOUSEEVENTF_LEFTDOWN = 0X0002;
+        private const int MOUSEEVENTF_LEFTOP = 0X0004;
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        //gera as posições aleatórias
+        private Random random = new Random();
+
+        private void MoverCursor()
+        {
+            int larguraTela = Screen.PrimaryScreen.Bounds.Width;
+            int alturaTela = Screen.PrimaryScreen.Bounds.Height;
+            int posicaoXAleatoria = random.Next(larguraTela);
+            int posicaoYaleatoria = random.Next(alturaTela);
+            Cursor.Position = new Point(posicaoXAleatoria, posicaoYaleatoria);
+        }
         public Form1()
         {
             InitializeComponent();
