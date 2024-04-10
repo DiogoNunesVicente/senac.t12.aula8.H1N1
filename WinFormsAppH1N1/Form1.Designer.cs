fc44688eef8c64e8d1593f42c46a44cd3a453f36
Diogo Nunes Vicente <diogonunesv742@gmail.com>
2024-04-09 21:06:20 -0300

    Aham

--- WinFormsAppH1N1/Form1.Designer.cs
+++ WinFormsAppH1N1/Form1.Designer.cs
@@ -28,12 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(287, 159);
+            button1.Name = "button1";
+            button1.Size = new Size(245, 145);
+            button1.TabIndex = 0;
+            button1.Text = "Seu PAU até 20cm maior ";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            DoubleBuffered = true;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
--- WinFormsAppH1N1/Form1.cs
+++ WinFormsAppH1N1/Form1.cs
@@ -5,9 +5,9 @@ namespace WinFormsAppH1N1
     public partial class Form1 : Form
     {
         private const int MOUSEEVENTF_LEFTDOWN = 0X0002;
-        private const int MOUSEEVENTF_LEFTOP = 0X0004;
-        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        private const int MOUSEEVENTF_LEFTUP = 0X0004;
 
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
         //gera as posições aleatórias
         private Random random = new Random();
@@ -23,6 +23,26 @@ namespace WinFormsAppH1N1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 200; i++)
+            {
+                MoverCursor();
+                Thread.Sleep(10);
+
+                for(int tico = 1; tico <= 2; tico++)
+                {
+                    MouseClicar();
+                }               
+            }
+        }
+
+        private void MouseClicar()
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, Cursor.Position.X, Cursor.Position.Y, 0 , 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, Cursor.Position.X, Cursor.Position.Y, 0, 0);
         }
     }
 }
